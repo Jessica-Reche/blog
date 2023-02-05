f2a239a91d0c5539d29e81aa813824c7d8f6c913
Jessica-Reche <you@example.com>
2023-02-05 23:56:58 +0100

    cambios realizados

--- app/Http/Middleware/RolCheck.php
+++ app/Http/Middleware/RolCheck.php
@@ -16,31 +16,42 @@ class RolCheck
      * @return \Illuminate\Http\Response|\Illuminate\Http\RedirectResponse
      */
     public function handle(Request $request, Closure $next, $rol, ...$roles)
-{
-    $route = $request->route()->getName();
-    $user = auth()->user();
-
-    if (in_array($user->rol, $roles)) {
-        return $next($request);
-    }
-
-    if ($user->rol === 'admin') {
-        if (in_array($route, ['posts.create', 'posts.store'])) {
+    {
+        $route = $request->route()->getName();
+        $user = auth()->user();
+        $existeUsuario = isset($user->rol);
+        if ($existeUsuario && in_array($user->rol, $roles)) {
             return $next($request);
         }
-    } elseif ($user->rol === 'editor') {
-        if (in_array($route, ['posts.edit', 'posts.update', 'posts.destroy'])) {
-            $post = Post::find($request->route()->parameter('post'));
-            if ($post->usuario_id == $user->id) {
-                return $next($request);
+        if ($existeUsuario) {
+            switch ($user->rol) {
+                case 'admin':
+                    if (in_array($route, ['posts.create', 'posts.store'])) {
+                        return $next($request);
+                    }
+                    break;
+                case 'editor':
+                    if (in_array($route, ['posts.edit', 'posts.update', 'posts.destroy'])) {
+                        $post = Post::find($request->route()->parameter('post'));
+                        if ($post->usuario_id == $user->id) {
+                            return $next($request);
+                        }
+                    } elseif (in_array($route, ['posts.create', 'posts.store'])) {
+                        return $next($request);
+                    }
+                    break;
+                default:
+                    if (in_array($user->rol, $roles)) {
+                        return $next($request);
+                    }
+                    break;
             }
-        } elseif (in_array($route, ['posts.create', 'posts.store'])) {
-            return $next($request);
         }
-    }
 
-    return redirect('/');
-}
+
+        return redirect('/');
+
+    }
 
 
 
